da2d07be4d9eb0ade274f9943e81708327ef933d
Daniel <dphillip11@gmail.com>
2023-06-12 20:35:21 +0200

    added culled flag

--- Test.cpp
+++ Test.cpp
@@ -1,131 +1,174 @@
-#include <iostream>
+#include <stdexcept>
+#include <unordered_map>
 #include <vector>
-#include "HashedVector.hpp"
-#include <cassert>
+#include <algorithm>
 
-void testHashedVector()
-{
-  bool testPassed = true;
+//a data structure that maintains a contiguous array of objects and a map of pointers to them
+template <typename T>
+class HashedVector {
 
-    HashedVector<int> hashedVector;
-
-    // Add elements to the HashedVector
-    int id1 = hashedVector.push_back(10);
-    int id2 = hashedVector.push_back(20);
-    int id3 = hashedVector.push_back(30);
-
-    // Modify and access elements using the [] operator
-    hashedVector[id1] = 100;
-    hashedVector[id2] = 200;
-    hashedVector[id3] = 300;
-
-    // Access elements using try_get and print their values
-    if (int* element1 = hashedVector.try_get(id1)) {
-        std::cout << "Element 1: " << *element1 << std::endl;
-    } else {
-        testPassed = false;
-        std::cout << "Failed to access Element 1 using try_get." << std::endl;
-    }
-    if (int* element2 = hashedVector.try_get(id2)) {
-        std::cout << "Element 2: " << *element2 << std::endl;
-    } else {
-        testPassed = false;
-        std::cout << "Failed to access Element 2 using try_get." << std::endl;
-    }
-    if (int* element3 = hashedVector.try_get(id3)) {
-        std::cout << "Element 3: " << *element3 << std::endl;
-    } else {
-        testPassed = false;
-        std::cout << "Failed to access Element 3 using try_get." << std::endl;
-    }
-
-    // Remove an element from the HashedVector
-    hashedVector.remove(id2);
-
-    // Try accessing the removed element, should return nullptr
-    if (int* removedElement = hashedVector.try_get(id2)) {
-        testPassed = false;
-        std::cout << "Element 2 still exists after removal. Test failed." << std::endl;
-    }
-
-    // Try accessing an invalid index, should throw an exception
-    try {
-        int invalidElement = hashedVector[999];
-        testPassed = false;
-        std::cout << "Invalid Element accessed without throwing an exception. Test failed." << std::endl;
-    } catch (const std::out_of_range&) {
-        // Exception caught as expected
-    }
-
-    // Check pass or fail condition
-    if (testPassed) {
-        std::cout << "All test cases passed!" << std::endl;
-    } else {
-        std::cout << "One or more test cases failed." << std::endl;
-    }
-
-     //keep a list of floats and their pointerss
-	std::vector<float*> std_vector_pointers;
-	std::vector<float> vector;
-	HashedVector<float> hashed_vector;
-
-	for (int i = 0; i < 10; i++)
+private:
+	//a pointer wrapper to check for deletions
+	struct _HashObject {
+		bool deleted = false;
+		bool culled = false;
+		T* t;
+	};
+	//a map to link IDs to t pointers
+	std::unordered_map<int, _HashObject> hashMap;
+	//the contiguous vector of underlying ts
+	std::vector<T> tVector;
+	//id counter
+	int next_ID = 0;
+	// bool to check if a cull is needed before rehash
+	bool cullNeeded = false;
+	//fix hash object pointers when vector reallocates
+	void Rehash()
 	{
-		vector.push_back(i * 1.0f);
-		std_vector_pointers.push_back(&vector.back());
-        auto id = hashed_vector.push_back(i * 1.0f);
-        assert(id == i);
+		if (cullNeeded)
+			CullDeleted();
+		int check_ID = 0;
+		for (int i = 0; i < tVector.size(); i++)
+		{
+			//ignored deleted ID's
+			while (hashMap[check_ID].deleted)
+			{
+				check_ID++;
+			}
+			//assign current address to has object
+			hashMap[check_ID].t = &tVector[i];
+			//increment ID
+			check_ID++;
+		}
 	}
 
-    auto Test = [](std::vector<float*>& std_vector_pointers, HashedVector<float>& hashed_vector){
-        int valid_vector_points = 0;
-        int valid_hashed_vector_points = 0;
-        //demonstrate pointers are invalid
-        for (int i = 0; i < 10; i++)
-        {
-            auto vector_success = *std_vector_pointers[i] == i * 1.0f;
-            auto hashed_vector_success = hashed_vector[i] == i * 1.0f;
-            valid_vector_points += vector_success;
-            valid_hashed_vector_points += hashed_vector_success;
-        }
-        std::cout << "Vector: " << (valid_vector_points) << std::endl;
-        std::cout << "HashedVector: " << (valid_hashed_vector_points) << std::endl;
-    };
+	//internal function to remove a single element from the vector, not error checked
+	void RemoveElementFromVector(int id)
+	{
+		T* elementToRemove = hashMap[id].t;
+		auto matchElement = [elementToRemove](const T& e) {
+			return &e == elementToRemove;};
+		auto eraseIt = std::find_if(tVector.begin(), tVector.end(), matchElement);
+		if (eraseIt != tVector.end()) {
+			tVector.erase(eraseIt);
+		}
+		hashMap[id].culled = true;
+	}
+public:
+	//adds the item to the vector and returns its key ID
+	int push_back(T t)
+	{
+		//check vector capacity
+		int capacity_before = tVector.capacity();
+		//set ID
+		int ID = next_ID;
+		//increment next ID
+		next_ID++;
+		//Add it to the vector
+		tVector.push_back(t);
+		_HashObject h;
+		h.t = &tVector.back();
+		//Add object to hashMap;
+		hashMap[ID] = h;
+		//rehash if vector capacity changed
+		if (tVector.capacity() != capacity_before)
+			Rehash();
+		return ID;
+	}
 
-	Test(std_vector_pointers, hashed_vector);
+	//removes the item from the vector, cull wil be performed on rehash if set to false
+	void remove(int id, bool cullVector = true)
+	{
+		//check if ID is valid
+		if (hashMap.find(id) == hashMap.end())
+			throw std::out_of_range("Invalid index assertion error: Index out of range.");
+		//check if ID is deleted
+		if (hashMap[id].deleted)
+			return;
+		//check if pointer is valid
+		if (hashMap[id].t == nullptr)
+			return;
+		// Mark object as deleted
+		hashMap[id].deleted = true;
 
-    //remove 5 elements
-    for (int i = 0; i < 5; i++)
-    {
-        vector.erase(vector.begin() + i);
-        hashed_vector.remove(i);
-    }
-    //add 5 elements
-    for (int i = 0; i < 5; i++)
-    {
-        vector.push_back(i * 1.0f);
-        auto id = hashed_vector.push_back(i * 1.0f);
-        assert(id == i + 10);
-    }
-    //now the data should be 5678901234
-    //valid id's should be 5-14
-    int expected[] = {5,6,7,8,9,0,1,2,3,4};
-    int valid_vector_points = 0;
-    int valid_hashed_vector_points = 0;
+		cullNeeded = true;
+		if (cullVector)
+			CullDeleted();
+	}
 
-    for (int i = 0; i < 10; i++)
-    {
-        auto vector_success = *std_vector_pointers[i] == expected[i] * 1.0f;
-        auto hashed_vector_success = hashed_vector[i + 5] == expected[i] * 1.0f;
-        valid_hashed_vector_points += hashed_vector_success;
-        valid_vector_points += vector_success;
-    }
-    std::cout << "Vector: " << (valid_vector_points) << std::endl;
-    std::cout << "HashedVector: " << (valid_hashed_vector_points) << std::endl;
-}
+	//cull deleted elements from the vector to maintain contiguousness
+	void CullDeleted()
+	{
+		//check if cull is needed
+		if (!cullNeeded)
+			return;
 
-int main(){
-    testHashedVector();
-    return 0;
+		// Iterate through the hash map and remove elements with element.deleted = true
+		for (auto it = hashMap.begin(); it != hashMap.end(); it++) {
+			if (it->second.deleted && !it->second.culled)
+				RemoveElementFromVector(it->first);
+		}
 
-}
+		// Reset cullNeeded flag
+		cullNeeded = false;
+		//rehash
+		Rehash();
+	}
+
+
+	//returns the item at the given index, will throw error if invalid
+	T& operator [](int id)
+	{
+		auto it = hashMap.find(id);
+		if (it == hashMap.end())
+			throw std::out_of_range("Invalid index assertion error: Index out of range.");
+		if (hashMap[id].deleted)
+			throw std::out_of_range("Invalid index assertion error: element deleted.");
+		return *hashMap[id].t;
+
+	}
+
+	//returns the item at the given index, will return nullptr if invalid
+	T* try_get(int id)
+	{
+		auto it = hashMap.find(id);
+		if (it == hashMap.end())
+			return nullptr;
+		if (hashMap[id].deleted)
+			return nullptr;
+		return hashMap[id].t;
+	}
+
+	void reserve(int size)
+	{
+		tVector.reserve(size);
+	}
+
+	auto size() const noexcept
+	{
+		return tVector.size();
+	}
+
+	bool empty() const noexcept
+	{
+		return tVector.empty();
+	}
+
+	void clear() noexcept
+	{
+		tVector.clear();
+		hashMap.clear();
+		next_ID = 0;
+	}
+
+	//returns a reference to the underlying vector
+	std::vector<T>& getVector()
+	{
+		if (cullNeeded)
+			CullDeleted();
+		return tVector;
+	}
+
+
+
+};
